1f753422168c7368065d3581a0d7006700b5ec45
Niclas Paulsson <niclas.paulsson@hotmail.com>
2019-04-07 22:06:51 +0100

    pass the expected values to the boxes

--- src/pi-memory-app/src/app/components/decimal-box/decimal-box.component.ts
+++ src/pi-memory-app/src/app/components/decimal-box/decimal-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-decimal-box',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DecimalBoxComponent implements OnInit {
 
-  expectedValue: number;
+  @Input() expectedValue: number;
   actualValue: number;
 
   constructor() { }
